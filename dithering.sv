15a0ab2c910c229024649c2e62f8eb5405f90baf
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-08-28 10:32:31 -0300

    Update dithering.sv

--- dithering.sv
+++ dithering.sv
@@ -1,1 +1,27 @@
-
+module dithering(
+	input logic [7:0] channel,
+	input logic SW,
+	output logic [7:0] d_channel
+);
+	//Menesteres
+	logic [3:0] MSN;
+	logic [1:0] MS2b;
+	logic [7:0] dithered;
+	//Control del Switch
+	always_comb begin
+		//defaults
+		d_channel = channel;
+		case(SW)
+			1'b1:	d_channel = dithered;
+			1'b0:	d_channel = channel;
+		endcase
+	end
+	
+	//Dithering
+	always_comb begin
+		MSN = channel[7:4];
+		MS2b = channel [3:2];
+		if ( MSN == 'hFF) begin
+			dithered = MSN;
+		end
+		
