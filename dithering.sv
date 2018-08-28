77b5864675e3e7145c8221b65652dc132d14e1c1
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-08-28 13:07:04 -0300

    Update dithering.sv

--- dithering.sv
+++ dithering.sv
@@ -1,8 +1,19 @@
-`timescale 1ns / 1ps
-
-module dithering(
-
-    );
+module dithering #(parameter THOLD=8)(
+    input logic [7:0] channel,
+    input logic SW,
+    output logic dithered
+);
+    always_comb begin
+        dithered = channel;
+        case(SW)
+            1'b0:   dithered = channel;
+            1'b1:   begin
+                if (channel[7:4] >'d15 && channel[3:0] >= THOLD) begin
+                    dithered = {channel[7:4] + 'd1, 'd0};
+                end
+                else begin
+                    dithered = channel;
+            end
+        endcase
+    end
     
-    
-endmodule
